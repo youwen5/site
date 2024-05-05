56c82574ec6947ab7a6da7c89e43296b6df7cacf
Youwen Wu <youwenw@gmail.com>
2024-05-05 10:55:32 -0700

    fix: correctly detect navigation state to external url

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -19,6 +19,8 @@
 		root = document.getElementsByTagName('html')[0];
 
 		root?.classList.add('smoothscroll');
+
+		navigating = false;
 	});
 
 	beforeNavigate(() => {
@@ -44,4 +46,5 @@
 	<slot />
 </div>
 
+<Footer />
 <Footer />
