c3a83c828806975ff04192b74b1f091d217d5ac6
Youwen Wu <youwenw@gmail.com>
2024-06-27 00:59:29 -0700

    fix: text spacing in contact info

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -107,8 +107,10 @@
         <Card.Content>
           <h3>Find me on:</h3>
           <Socials />
-          I read message requests everywhere but do not accept Discord friend requests if I don't know
-          you. The best way to reach me and receive a response is probably by email.
+          <p class="mt-4">
+            I read message requests everywhere but do not accept Discord friend requests if I don't
+            know you. The best way to reach me and receive a response is probably by email.
+          </p>
         </Card.Content>
       </Card.Root>
     </div>
